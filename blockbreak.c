f069cf867f109145bf176a4900e536f708b87a33
GwonGichan <gichan8420@naver.com>
2023-11-13 10:05:40 +0900

    위치 이동 dx,dy값 작성

--- blockbreak.c
+++ blockbreak.c
@@ -9,6 +9,7 @@ void gotoxy(int x, int y) {//원하는 x,y좌표로 이동시키는 함수
 int main(){
     int x = 1;  // 공의 초기 x 좌표
     int y = 1;  // 공의 초기 y 좌표
-   
+    int dx = 1; // 공을 오른쪽으로 이동시킴
+    int dy = 1; // 공을 아래쪽으로 이동시킴
 
 }
