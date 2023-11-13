fe3c3740b9ee7cb689f0cadaf8e94da94fb61fd3
04kwon <kwonsegwang@naver.com>
2023-11-14 00:10:09 +0900

    막대 길이 수정

--- blockbreak.c
+++ blockbreak.c
@@ -11,10 +11,14 @@ int main(){
     int y = 1;  // 공의 초기 y 좌표
     int dx = 1; // 공을 오른쪽으로 이동시킴
     int dy = 1; // 공을 아래쪽으로 이동시킴
-    int bar1_x = 19; //왼쪽 막대 초기 x 좌표
+    int bar1_x = 18; //왼쪽 막대 초기 x 좌표
     int bar1_y = 45; //왼쪽 막대 초기 y 좌표
-    int bar2_x = 20; //오른쪽 막대 초기 x 좌표
-    int bar2_y = 45; //오른쪽 막대 초기 y 좌표
+    int bar2_x = 19;
+    int bar2_y = 45;
+    int bar3_x = 20; //오른쪽 막대 초기 x 좌표
+    int bar3_y = 45; //오른쪽 막대 초기 y 좌표
+    int bar4_x = 21;
+    int bar4_y = 45;
 
     system("mode con cols=40 lines=50"); // 콘솔 창 크기 설정
 
@@ -27,10 +31,13 @@ int main(){
         gotoxy(x, y);
         printf("■");//위치에 따라 공 출력
         gotoxy(bar1_x, bar1_y);
-        printf("■");//위치에 따라 왼쪽 막대 출력
+        printf("O");//위치에 따라 왼쪽 막대 출력
         gotoxy(bar2_x, bar2_y);
-        printf("■");//위치에 따라 오른쪽 막대 출력
-
+        printf("O");
+        gotoxy(bar3_x, bar3_y);
+        printf("O");//위치에 따라 오른쪽 막대 출력
+        gotoxy(bar4_x, bar4_y);
+        printf("O");
         Sleep(100);//공이 이동하는 속도 조절
 
         if(x==1||x==39){//특정 x좌표에서 이동방향 반대로 바꾸기
@@ -43,17 +50,29 @@ int main(){
             dx = -1;
             dy = -1;
         }
-        if (x == bar2_x && y == bar2_y-1) {//공이 오른쪽 막대에 닿으면 오른쪽으로 보내기
+        if (x == bar2_x && y == bar2_y-1) {
+            dx = -1;
+            dy = -1;
+        }
+        if (x == bar3_x && y == bar3_y - 1) {//공이 왼쪽 막대에 닿으면 왼쪽으로 보내기
+            dx = 1;
+            dy = -1;
+        }
+        if (x == bar4_x && y == bar4_y - 1) {
             dx = 1;
             dy = -1;
         }
         if (GetAsyncKeyState(VK_LEFT) & 0x8000) { // 막대 왼쪽으로 이동
             bar1_x--;
             bar2_x--;
+            bar3_x--;
+            bar4_x--;
         }
         if (GetAsyncKeyState(VK_RIGHT) & 0x8000) { // 막대 오른쪽으로 이동
             bar1_x++;
             bar2_x++;
+            bar3_x++;
+            bar4_x++;
         }
     }
 }
