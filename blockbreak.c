175bad8929809aaa83399f48946d64aa9f2f6521
GwonGichan <gichan8420@naver.com>
2023-11-13 09:59:12 +0900

    gotoxy 함수 작성

--- blockbreak.c
+++ blockbreak.c
@@ -1,7 +1,13 @@
 #include <stdio.h>
 #include <windows.h>
-int main(){
-   
 
+void gotoxy(int x, int y) {//원하는 x,y좌표로 이동시키는 함수
+    COORD pos = { x, y };
+    SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), pos);
+}
+ 
+int main(){
+
+   
 
 }
