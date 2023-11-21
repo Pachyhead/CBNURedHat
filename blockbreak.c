03aabee936f0b33fcc13fcd1d67c7a8241f680e9
GwonGichan <gichan8420@naver.com>
2023-11-21 11:27:23 +0900

    공의 초기 좌표 수정

--- blockbreak.c
+++ blockbreak.c
@@ -7,8 +7,8 @@ void gotoxy(int x, int y) {//원하는 x,y좌표로 이동시키는 함수
 }
  
 int main(){
-    int x = 1;  // 공의 초기 x 좌표
-    int y = 1;  // 공의 초기 y 좌표
+    int x = 2;  // 공의 초기 x 좌표
+    int y = 2;  // 공의 초기 y 좌표
     int dx = 1; // 공을 오른쪽으로 이동시킴
     int dy = 1; // 공을 아래쪽으로 이동시킴
     int bar1_x = 18; //왼쪽 막대 초기 x 좌표
