5c6fb2292218a9949c5978520b98c6c6381798b1
GwonGichan <gichan8420@naver.com>
2023-12-08 21:24:08 +0900

    문제 줄바꾸기 수정

--- capital and song quiz.c
+++ capital and song quiz.c
@@ -60,16 +60,16 @@ int main(void) {
 	"omg", "next level", "fast forward", "좋니", "오래된 노래"};
 	char quiz[20][200] = {"한국","일본", "중국", "미국", "이탈리아", 
 	"프랑스", "독일", "그리스", "포르투갈", "스페인", 
-	"Weight of thee world on your shoulders\nI'll kiss your waist and ease your mind",
-	"Imma baddie baddie baddie\nPretty little risky baddie",
-	"떠나는 그대여 울지 말아요 슬퍼 말아요\n내가 단념할게요 마음 편히 가시도록",
-	"사랑아 왜 도망가\n수줍은 아이처럼",
-	"Cause I know what you like boy\nYou're my chemical hype boy",
-	"Oh my oh my God\n예상했어 나",
-	"I see the NU EVO.\n적대적인 고난과 슬픔은",
-	"How many lovers do I go through to find you\nHow many how many how many",
-	"이제 괜찮니 너무 힘들었잖아\n우리 그 마무리가",
-	"오래전에 함께듣던 노래가\n거리에서 내게 우연히 들려온것처럼"};
+	"Weight of thee world on your shoulders I'll kiss your waist and ease your mind",
+	"Imma baddie baddie baddie Pretty little risky baddie",
+	"떠나는 그대여 울지 말아요 슬퍼 말아요 내가 단념할게요 마음 편히 가시도록",
+	"사랑아 왜 도망가 수줍은 아이처럼",
+	"Cause I know what you like boy You're my chemical hype boy",
+	"Oh my oh my God 예상했어 나",
+	"I see the NU EVO. 적대적인 고난과 슬픔은",
+	"How many lovers do I go through to find you How many how many how many",
+	"이제 괜찮니 너무 힘들었잖아 우리 그 마무리가",
+	"오래전에 함께듣던 노래가 거리에서 내게 우연히 들려온것처럼"};
 	char input[24] = {""};
 	int n, cnt=0;
 	char ch;
